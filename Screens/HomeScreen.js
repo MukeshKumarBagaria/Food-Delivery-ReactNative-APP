b089ae034cc3d20eb3d1d8fe8a123967ed75b366
mukesh bagaria <mukeshbagaria@mukeshs-MacBook-Air.local>
2023-05-11 14:06:31 +0530

    completed user search bar section

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { View, Image, Text } from 'react-native'
+import { View, Image, Text, TextInput, ScrollView } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
 import { SafeAreaView } from 'react-native-safe-area-context';
@@ -15,9 +15,8 @@ export default function HomeScreen() {
     return (
         <SafeAreaView className="bg-white" >
             {/* Header which includes logo and search bar*/}
-            <View className="flex-row mx-2 my-3 space-x-2">
+            <View className="flex-row mx-3 my-3 space-x-2 items-center ">
                 <Image
-
                     source={{
                         uri: 'https://picsum.photos/200/300',
                     }}
@@ -31,9 +30,27 @@ export default function HomeScreen() {
                     </Text>
 
                 </View>
-                <Icon name="user-circle"  size={30} color="#00CCBB"/>
+                <Icon name="user-circle" size={25} color="#00CCBB" />
+            </View>
+
+            {/* Search Bar */}
+            <View className="flex-row mx-4 space-x-3 align-middle pb-2 items-center">
+                <View className="flex-row space-x-2 flex-1 bg-gray-200 p-2 rounded align-middle">
+                    <Icon name="search" size={20} color="#00CCBB" />
+                    <TextInput placeholder='Search for food!' />
+                </View>
+                <Icon name="filter" size={25} color="#00CCBB" />
 
             </View>
+
+
+            {/* Body */}
+            <ScrollView>
+                {/* Categories section */}
+
+
+                {/* Featured Section */}
+            </ScrollView>
         </SafeAreaView>
     )
 }
